ae2590bd0a07cb5bf3204bc1c385b47ba0ce370b
David Sauvage <davidsauvage8@gmail.com>
2011-02-28 16:26:06 +0100

    Basics functions are shorter
    -delete the of prefix
    -function now have common abreviations (gt lt etc...)

--- src/basics.py
+++ src/basics.py
@@ -37,49 +37,49 @@ def __basic_comparison(operator, *arguments_list):
       raise TypeError, "Arguments must be Numbers, strings or CellReferences"
 
 #Public API
-def of_add(*number_list):
+def add(*number_list):
     """Verify arguments and return a string for an "add" expression """
     return __basic_operation("+", *number_list)
 
-def of_substract(*number_list):
+def sub(*number_list):
     """Verify arguments and return a string for a "substract" expression """
     return __basic_operation("-", *number_list)
 
-def of_multiply(*number_list):
+def mul(*number_list):
     """Verify arguments and return a string for a "multiply" expression """
     return __basic_operation("*", *number_list)
 
-def of_divide(*number_list):
+def div(*number_list):
     """Verify arguments and return a string for a "divide" expression """
     return __basic_operation("/", *number_list)
 
-def of_concatenate(*string_list):
+def concatenate(*string_list):
     """Verify arguments and return a string for a "concatenate" expression """
     if is_string_list(string_list):
         return "&".join(str(i) for i in string_list)
     else:
         raise TypeError, "Arguments must be strings"
 
-def of_equal(*arguments_list):
+def eq(*arguments_list):
     """Verify arguments and return a string for an "equal (=)" expression """
     return __basic_comparison("=", *arguments_list)
 
-def of_different(*arguments_list):
+def ne(*arguments_list):
     """Verify arguments and return a string for an "different (<>)" expression """
     return __basic_comparison("<>", *arguments_list)
 
-def of_upper(*arguments_list):
+def gt(*arguments_list):
     """Verify arguments and return a string for an "upper (>)" expression """
     return __basic_comparison(">", *arguments_list)
 
-def of_lower(*arguments_list):
+def lt(*arguments_list):
     """Verify arguments and return a string for an "lower (<)" expression """
     return __basic_comparison("<", *arguments_list)
 
-def of_upper_equal(*arguments_list):
+def ge(*arguments_list):
     """Verify arguments and return a string for an "upper or equal (>=)" expression """
     return __basic_comparison(">=", *arguments_list)
 
-def of_lower_equal(*arguments_list):
+def le(*arguments_list):
     """Verify arguments and return a string for an "lower or equal (<=)" expression """
     return __basic_comparison("<=", *arguments_list)
--- src/test_basics.py
+++ src/test_basics.py
@@ -1,79 +1,79 @@
 import unittest
 
 from objects import CellReference, Number
-from basics import of_add, of_substract, of_multiply, of_divide
-from basics import of_concatenate, of_equal, of_different, of_upper
-from basics import of_lower, of_upper_equal, of_lower_equal
+from basics import add, sub, mul, div
+from basics import concatenate, eq, ne, gt
+from basics import lt, ge, le
 
 class TestOpenFormulaBasicsOp(unittest.TestCase):
     
 #Basics operations
     def test_of_add(self):
-        self.assertEqual(of_add(CellReference("[.A1]")), "[.A1]")
-        self.assertEqual(of_add(Number("2"), CellReference("[.D4]")), "2+[.D4]")
-        self.assertRaises(TypeError, of_add, 2)
+        self.assertEqual(add(CellReference("[.A1]")), "[.A1]")
+        self.assertEqual(add(Number("2"), CellReference("[.D4]")), "2+[.D4]")
+        self.assertRaises(TypeError, add, 2)
 
     def test_of_substract(self):
-        self.assertEqual(of_substract(CellReference("[.A1]")), "[.A1]")
-	self.assertEqual(of_substract(Number("2"), CellReference("[.D4]")),
+        self.assertEqual(sub(CellReference("[.A1]")), "[.A1]")
+	self.assertEqual(sub(Number("2"), CellReference("[.D4]")),
                                                                       "2-[.D4]")
 
-        self.assertRaises(TypeError, of_substract, 2)
+        self.assertRaises(TypeError, sub, 2)
    
     def test_of_multiply(self):
-        self.assertEqual(of_multiply(CellReference("[.A1]")), "[.A1]")
-	self.assertEqual(of_multiply(Number("2"), CellReference("[.D4]")),
+        self.assertEqual(mul(CellReference("[.A1]")), "[.A1]")
+	self.assertEqual(mul(Number("2"), CellReference("[.D4]")),
                                                                       "2*[.D4]")
 
-        self.assertRaises(TypeError, of_multiply, 2)
+        self.assertRaises(TypeError, mul, 2)
 
     def test_of_divide(self):
-        self.assertEqual(of_divide(CellReference("[.A1]")), "[.A1]")
-	self.assertEqual(of_divide(Number("2"), CellReference("[.D4]")),
+        self.assertEqual(div(CellReference("[.A1]")), "[.A1]")
+	self.assertEqual(div(Number("2"), CellReference("[.D4]")),
                                                                       "2/[.D4]")
 
-        self.assertRaises(TypeError, of_divide, 2)
+        self.assertRaises(TypeError, div, 2)
 
     def test_of_concatenate(self):
-        self.assertEqual(of_concatenate("abc"), "abc")
-        self.assertEqual(of_concatenate("abc","def","ghi"),"abc&def&ghi")
-        self.assertRaises(TypeError, of_concatenate, 2)
+        self.assertEqual(concatenate("abc"), "abc")
+        self.assertEqual(concatenate("abc","def","ghi"),"abc&def&ghi")
+        self.assertRaises(TypeError, concatenate, 2)
 
     def test_of_equal(self):
-        self.assertEqual(of_equal("abc").str, "abc")
-        self.assertEqual(of_equal("abc","def","ghi").str,"abc=def=ghi")
-        self.assertEqual(of_equal(Number("2"), Number("3")).str,"2=3")
-        self.assertRaises(TypeError, of_equal, 2) 
+        self.assertEqual(eq("abc").str, "abc")
+        self.assertEqual(eq("abc","def","ghi").str,"abc=def=ghi")
+        self.assertEqual(eq(Number("2"), Number("3")).str,"2=3")
+        self.assertRaises(TypeError, eq, 2) 
 
     def test_of_different(self):
-        self.assertEqual(of_different("abc").str, "abc")
-        self.assertEqual(of_different("abc","def","ghi").str,"abc<>def<>ghi")
-        self.assertEqual(of_different(Number("2"), Number("3")).str,"2<>3")
-        self.assertRaises(TypeError, of_different, 2) 
+        self.assertEqual(ne("abc").str, "abc")
+        self.assertEqual(ne("abc","def","ghi").str,"abc<>def<>ghi")
+        self.assertEqual(ne(Number("2"), Number("3")).str,"2<>3")
+        self.assertRaises(TypeError, ne, 2) 
 
     def test_of_upper(self):
-        self.assertEqual(of_upper("abc").str, "abc")
-        self.assertEqual(of_upper("abc","def","ghi").str,"abc>def>ghi")
-        self.assertEqual(of_upper(Number("2"), Number("3")).str,"2>3")
-        self.assertRaises(TypeError, of_different, 2) 
+        self.assertEqual(gt("abc").str, "abc")
+        self.assertEqual(gt("abc","def","ghi").str,"abc>def>ghi")
+        self.assertEqual(gt(Number("2"), Number("3")).str,"2>3")
+        self.assertRaises(TypeError, gt, 2) 
 
     def test_of_lower(self):
-        self.assertEqual(of_lower("abc").str, "abc")
-        self.assertEqual(of_lower("abc","def","ghi").str,"abc<def<ghi")
-        self.assertEqual(of_lower(Number("2"), Number("3")).str,"2<3")
-        self.assertRaises(TypeError, of_lower, 2) 
+        self.assertEqual(lt("abc").str, "abc")
+        self.assertEqual(lt("abc","def","ghi").str,"abc<def<ghi")
+        self.assertEqual(lt(Number("2"), Number("3")).str,"2<3")
+        self.assertRaises(TypeError, lt, 2) 
 
     def test_of_upper_equal(self):
-        self.assertEqual(of_upper_equal("abc").str, "abc")
-        self.assertEqual(of_upper_equal("abc","def","ghi").str,"abc>=def>=ghi")
-        self.assertEqual(of_upper_equal(Number("2"), Number("3")).str,"2>=3")
-        self.assertRaises(TypeError, of_different, 2) 
+        self.assertEqual(ge("abc").str, "abc")
+        self.assertEqual(ge("abc","def","ghi").str,"abc>=def>=ghi")
+        self.assertEqual(ge(Number("2"), Number("3")).str,"2>=3")
+        self.assertRaises(TypeError, ge, 2) 
 
     def test_of_lower_equal(self):
-        self.assertEqual(of_lower_equal("abc").str, "abc")
-        self.assertEqual(of_lower_equal("abc","def","ghi").str,"abc<=def<=ghi")
-        self.assertEqual(of_lower_equal(Number("2"), Number("3")).str,"2<=3")
-        self.assertRaises(TypeError, of_lower_equal, 2) 
+        self.assertEqual(le("abc").str, "abc")
+        self.assertEqual(le("abc","def","ghi").str,"abc<=def<=ghi")
+        self.assertEqual(le(Number("2"), Number("3")).str,"2<=3")
+        self.assertRaises(TypeError, le, 2) 
 
 if __name__ == '__main__':
     unittest.main()
